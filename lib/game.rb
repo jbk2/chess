9f543b5caa90bea0004f1ddbffa1026e99a7a218
jbk2 <james@humanbenefits.com>
2023-06-02 16:38:28 +0200

    Further format script, class vars to constants, string formatting into convenience methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,15 +4,14 @@ require_relative 'board'
 require_relative 'player'
 
 class Game
-  attr_accessor :player_1, :player_2, :active_player
+  attr_accessor :player1, :player2, :active_player
   attr_reader :board
 
-  @@cmd_format = "\e[47m\e[0;30m"
-  @@blue = "\e[34m"
-  @@green = "\e[32m"
-  @@bold_white = "\e[1;37m"
-  @@cyan = "\e[0;36m"
-  @@ansi_end = "\e[0m"
+  BLUE = "\e[34m"
+  GREEN = "\e[32m"
+  BOLD_WHITE = "\e[1;37m"
+  CYAN = "\e[0;36m"
+  ANSI_END = "\e[0m"
   @@type_speed = 0.02
 
   def initialize
@@ -27,13 +26,13 @@ class Game
   end
 
   def create_players
-    display_string(yaml_variables['game']['welcome'], 0.01)
-    display_string(yaml_variables['game']['player1_name_prompt'], 0.01)
-    @player_1 = Player.new(get_player1_name)
-    display_string(ERB.new(yaml_variables['game']['player1_greeting']).result(binding), @@type_speed)
+    display_string(yaml_data['game']['welcome'], 0.01)
+    display_string(yaml_data['game']['player1_name_prompt'], 0.01)
+    @player1 = Player.new(get_player1_name)
+    display_string(ERB.new(yaml_data['game']['player1_greeting']).result(binding), @@type_speed)
     sleep 0.3
-    @player_2 = Player.new(get_player2_name)
-    display_string(ERB.new(yaml_variables['game']['player2_greeting']).result(binding), @@type_speed)
+    @player2 = Player.new(get_player2_name)
+    display_string(ERB.new(yaml_data['game']['player2_greeting']).result(binding), @@type_speed)
   end
 
   def play_game
@@ -45,30 +44,30 @@ class Game
   end
   
   def assign_colour
-    display_string(yaml_variables['game']['assigning_colour1'], @@type_speed)
-    player_1.colour = random_colour; sleep 1;
-    display_string(ERB.new(yaml_variables['game']['player1_colour_stmnt']).result(binding), @@type_speed)
-    player_1.colour == :white ? player_2.colour = :black : player_2.colour = :white; sleep 0.5;
-    display_string(ERB.new(yaml_variables['game']['player2_colour_stmnt']).result(binding), @@type_speed); sleep 0.7
+    display_string(yaml_data['game']['assigning_colour1'], @@type_speed)
+    player1.colour = random_colour; sleep 1;
+    display_string(ERB.new(yaml_data['game']['player1_colour_stmnt']).result(binding), @@type_speed)
+    player1.colour == :white ? player2.colour = :black : player2.colour = :white; sleep 0.5;
+    display_string(ERB.new(yaml_data['game']['player2_colour_stmnt']).result(binding), @@type_speed); sleep 0.7
   end
 
   def game_setup
     display_string("here's your board:\n", @@type_speed); sleep 0.7;
-    display_string(ERB.new(yaml_variables['game']['black_piece_instructions']).result(binding), @@type_speed); sleep 0.8;
+    display_string(ERB.new(yaml_data['game']['black_piece_instructions']).result(binding), @@type_speed); sleep 0.8;
     board.display_board_utf; sleep 1;
-    display_string(ERB.new(yaml_variables['game']['white_piece_instructions']).result(binding), @@type_speed)
+    display_string(ERB.new(yaml_data['game']['white_piece_instructions']).result(binding), @@type_speed)
     puts "\n"; sleep 3;
-    display_string(yaml_variables['game']['board_illustration'], @@type_speed)
+    display_string(yaml_data['game']['board_illustration'], @@type_speed)
     puts "\n"; sleep 2;
   end
   
   
   def start_game
-    display_string(ERB.new(yaml_variables['game']['turn_instructions']).result(binding), @@type_speed)
-    # turn_instructions = yaml_variables['game']['turn_instructions']
+    display_string(ERB.new(yaml_data['game']['turn_instructions']).result(binding), @@type_speed)
+    # turn_instructions = yaml_data['game']['turn_instructions']
     # formatted_instructions = turn_instructions.encode('UTF-8')
     # display_string(formatted_instructions, @@type_speed)
-    # puts yaml_variables['game']['turn_instructions']
+    # puts yaml_data['game']['turn_instructions']
     
   end
   
@@ -77,26 +76,34 @@ class Game
   end
   
   def toggle_turn
-    active_player == player_1 ? (self.active_player = player_2) : (self.active_player = player_1)
+    active_player == player1 ? (self.active_player = player2) : (self.active_player = player1)
   end
   
+  
+  private
   def white_player
-    player_1.colour == :white ? player_1 : player_2
+    player1.colour == :white ? player1 : player2
   end
   
   def black_player
-    player_1.colour == :black ? player_1 : player_2
+    player1.colour == :black ? player1 : player2
+  end
+
+  def white_player_name
+    player1.colour == :white ? player1_name : player2_name
   end
   
-  private
+  def black_player_name
+    player1.colour == :black ? player1_name : player2_name
+  end
 
   def colour_emoji(colour)
     colour == :black ? "\u{26AB}" : "\u{26AA}"
   end
 
-  def yaml_variables
-    yaml_file = File.join(__dir__, "variables.yml")
-    variables = YAML.load_file(yaml_file)
+  def yaml_data
+    yaml_file = File.join(__dir__, "data.yml")
+    data = YAML.load_file(yaml_file)
   end
 
   def display_string(string, delay)
@@ -115,11 +122,11 @@ class Game
     $stdin.gets.chomp
   end
   def player1_name
-    player_1.name.capitalize
+    BLUE + player1.name.capitalize + ANSI_END
   end
   
   def player2_name
-    player_2.name.capitalize
+    GREEN + player2.name.capitalize + ANSI_END
   end
   
   def random_colour
