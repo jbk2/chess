328e65dc5389d5a0f5d5c9e29a1667885ef9b242
jbk2 <james@humanbenefits.com>
2023-06-13 12:22:36 +0200

    Write #queen_valid_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -27,4 +27,21 @@ class Queen < Piece
   def piece_valid_move?(move)
     # test whether given move is a valid move according to the piece's possible valid moves and a chess board of always 8x8
   end
+
+  def valid_queen_moves
+    down_right = (x+1..7).zip(y+1..7).delete_if { |e| e.include?(nil) }
+    down_left = (x+1..7).zip((0..y-1).to_a.reverse).delete_if {|e| e.include?(nil) }
+    up_right = ((0..x-1).to_a.reverse).zip(y+1..7).delete_if { |e| e.include?(nil) }
+    up_left = ((0..x-1).to_a.reverse).zip((0..y-1).to_a.reverse).delete_if { |e| e.include?(nil) }
+
+    up = (0...x).zip(Array.new(x, y))
+    down = (x+1..7).zip(Array.new(8-x, y))
+    left = Array.new(y, x).zip(0..y)
+    right = Array.new(7-y, x).zip(y+1..7)
+
+    diagonal_moves = down_right + down_left + up_right + up_left
+    straight_moves = up + down + left + right
+
+    diagonal_moves + straight_moves
+  end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -23,4 +23,72 @@ describe Queen do
       expect(white_queen.first_move?).to be(false)
     end
   end
+
+  describe '#valid_queen_move' do
+    context 'with a queen in square 0,3' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 0,3)
+        result = white_queen.valid_queen_moves
+        expect(result).to include([1, 4], [2, 5], [3, 6], [4, 7], [1, 2],
+          [2, 1], [3, 0], [0, 4], [0, 5], [0, 6], [0, 7], [0, 2], [0, 1], [0, 0],
+          [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3])
+      end
+    end
+    
+    context 'with a queen in square 4,3' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 4,3)
+        result = white_queen.valid_queen_moves
+        expect(result).to eq([[5, 4], [6, 5], [7, 6], [5, 2], [6, 1], [7, 0],
+        [3, 4], [2, 5], [1, 6], [0, 7], [3, 2], [2, 1], [1, 0], [0, 3], [1, 3],
+        [2, 3], [3, 3], [5, 3], [6, 3], [7, 3], [4, 0], [4, 1], [4, 2], [4, 4],
+        [4, 5], [4, 6], [4, 7]])
+      end
+    end
+    
+    context 'with a queen in square 3,3' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 3,3)
+        result = white_queen.valid_queen_moves
+        expect(result).to include([4, 4], [5, 5], [6, 6], [7, 7], [4, 2], [5, 1], [6, 0],
+          [2, 4], [1, 5], [0, 6], [2, 2], [1, 1], [0, 0], [2, 3], [1, 3], [0, 3], [4, 3],
+          [5, 3], [6, 3], [7, 3], [3, 2], [3, 1], [3, 0], [3, 4], [3, 5], [3, 6], [3, 7])
+        expect(result.length).to be(27)
+      end
+    end
+   
+    context 'with a queen in square 2,5' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 2,5)
+        result = white_queen.valid_queen_moves
+        expect(result).to include([3,6], [4,7], [3,4], [4,3], [5, 2], [6, 1],
+        [7, 0], [1, 6], [0, 7], [1, 4], [0, 3], [1, 5], [0, 5], [3, 5], [4, 5], [5, 5],
+        [6, 5], [7, 5], [2, 4], [2, 3], [2, 2], [2, 1], [2, 0], [2, 6], [2, 7])
+        expect(result.length).to be(25)
+      end
+    end
+    
+    context 'with a queen in square 5,3' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 5,3)
+        result = white_queen.valid_queen_moves
+        expect(result).to include([6, 4], [7, 5], [6, 2], [7, 1], [4, 4], [3, 5],
+        [2, 6], [1, 7], [4, 2], [3, 1], [2, 0], [4, 3], [3, 3], [2, 3], [1, 3], [0, 3],
+        [6, 3], [7, 3], [5, 2], [5, 1], [5, 0], [5, 4], [5, 5], [5, 6], [5, 7])
+        expect(result.length).to be(25)
+      end
+    end
+    
+    context 'with a queen in square 7,7' do
+      it 'returns all correct possible piece moves on an 8x8 board' do
+        white_queen = Queen.new(:white, 7,7)
+        result = white_queen.valid_queen_moves
+        expect(result).to include([6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1], [0, 0],
+        [6, 7], [5, 7], [4, 7], [3, 7], [2, 7], [1, 7], [0, 7], [7, 6], [7, 5], [7, 4],
+        [7, 3], [7, 2],  [7, 1], [7, 0])
+        expect(result.length).to be(21)
+      end
+    end
+
+  end
 end
